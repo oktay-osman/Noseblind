c9e35654e7a22ee38b6f978b4ca5e4fc06287e77
oktay-osman <oktai.osman21@gmail.com>
2024-02-03 19:16:17 +0200

    added models

---
+++ src/main/java/com/noseblind/model/Perfume.java
@@ -0,0 +1,39 @@
+package com.noseblind.model;
+
+import java.math.BigDecimal;
+
+public class Perfume {
+    private String name;
+    private BigDecimal price;
+    private String description;
+
+    public Perfume (String name, BigDecimal price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice () {
+        return price;
+    }
+
+    public void setPrice (BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+}
