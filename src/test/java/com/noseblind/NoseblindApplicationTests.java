fccccf77df3b68e3a46fbe924027e34f8fee4af4
oktay-osman <oktai.osman21@gmail.com>
2024-02-03 18:22:28 +0200

    initial commit

---
+++ src/test/java/com/noseblind/NoseblindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noseblind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoseblindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
